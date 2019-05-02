ba0ef7c084c8d1493383908f72c560f1d9f4c963
Defeng Kong <vergildfkong@hotmail.com>
2019-05-02 19:42:41 -0400

    Initial commit

---
+++ Assets/__Scripts/IKeyMaster.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IKeyMaster
+{
+    int keyCount { get; set; }                                                // a
+    int GetFacing();                                                          // b
+}
+
